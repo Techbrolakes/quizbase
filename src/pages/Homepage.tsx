46d96c5384a02e823525cec964d91987050244a9
Techbrolakes <lekandar11@gmail.com>
2023-07-03 20:57:33 +0100

    Feat: Project Structure Configs

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage: React.FC = () => {
+   return (
+      <div>
+         <h1 className="text-4xl">Homepage</h1>
+      </div>
+   );
+};
+
+export default Homepage;
