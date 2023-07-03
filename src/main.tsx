eca8ada781efe8bf61b104a259fbd0cc852f1ceb
Techbrolakes <lekandar11@gmail.com>
2023-07-03 20:59:18 +0100

    Feat: Project Structure Configs

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './config/routes';
-import './styles/index.css';
+import './config/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
    <React.StrictMode>
