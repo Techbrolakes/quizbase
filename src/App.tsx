ddb944c3cf7d084de5a5e584324cfc23ffa8406e
Techbrolakes <lekandar11@gmail.com>
2023-07-03 20:49:57 +0100

    Feat: Project Structure Configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from './baseui/ui/button';
 
 const App: React.FC = () => {
    return (
       <div>
          <h1 className="text-8xl font-bold">Hello world!</h1>
+         <Button>Click me</Button>
       </div>
    );
 };
