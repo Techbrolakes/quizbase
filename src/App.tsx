8cbf4212954b321748f3ef7a8c24ca6a1be33c35
Techbrolakes <lekandar11@gmail.com>
2023-07-03 20:36:24 +0100

    Feat: Project Structure Configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const App: React.FC = () => {
-  return <div>App</div>;
+   return (
+      <div>
+         <h1 className="text-8xl font-bold">Hello world!</h1>
+      </div>
+   );
 };
 
 export default App;
