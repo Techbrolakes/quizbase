d8ed947087e41ac2151a038f07715b738d644fb7
Techbrolakes <lekandar11@gmail.com>
2023-07-04 09:17:50 +0100

    Feat: Project Structure Configs

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import RootLayout from '@/config/layout/RootLayout';
+import React from 'react';
+
+const Homepage: React.FC = () => {
+   return (
+      <RootLayout>
+         <div className="text-5xl font-bold">Homepage</div>
+      </RootLayout>
+   );
+};
+
+export default Homepage;
