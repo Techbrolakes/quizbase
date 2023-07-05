f2694f6030570498592b70831207527654005a9a
Techbrolakes <lekandar11@gmail.com>
2023-07-05 13:39:24 +0100

    Feat: Quizbase intro section configs

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -22,7 +22,7 @@ const Intro: React.FC = () => {
                         <li className="my-6 text-sm lg:text-base font-medium tracking-wide leading-relaxed font-cabin">{rule}</li>
                      </ul>
                   ))}
-                  <Button className="mx-auto bg-blue-900 hover:bg-blue-800 transition-all duration-200 ease-linear hover:scale-105 hover:-rotate-2 font-cabin font-bold text-base w-1/3 block">
+                  <Button className="mx-auto px-12 bg-blue-900 hover:bg-blue-800 transition-all duration-200 ease-linear hover:scale-105 hover:-rotate-2 font-cabin font-bold text-base block">
                      Start Quiz
                   </Button>
                </div>
