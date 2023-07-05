1d507a7c5e9f114a6e661aa1af3b6cb39e4ad9c6
Techbrolakes <lekandar11@gmail.com>
2023-07-05 11:13:41 +0100

    Feat: Added Satoshi Fonts

---
+++ src/components/Intro.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Intro: React.FC = () => {
+   return <div className="flex flex-col justify-center items-center min-h-screen">Intro</div>;
+};
+
+export default Intro;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import RootLayout from '@/config/RootLayout';
-import { Button } from '@/baseui/ui/button';
+import Intro from '@/components/Intro';
 
 const Homepage: React.FC = () => {
    return (
       <RootLayout>
-         <div>
-            <h1 className="text-9xl">Hello World</h1>
-            <Button>Click me</Button>
-         </div>
+         <Intro />
       </RootLayout>
    );
 };
